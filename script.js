3feced48d3de883b37c88b9eed9be0049ed837ed
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2024-12-27 23:46:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let value of str){
+		let map = new map
+		if(map.includes(value)){
+			let t = map.get(value)
+			map.set(value,t+1)
+		}
+		else{
+			map.set(value,1)
+		}
+	}
+	let count = 0;
+	for(let [key,value] of map){
+		if(value==1){
+			return key
+			break;
+		}
+		else{
+			count++
+		}
+	}
+	if(count>1){
+		return null
+	}
+
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
